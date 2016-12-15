38115aa4124b4d811be07b1b09cb2fb2c5d1f71c
zanjs <root@zanjs.com>
2016-12-15 18:14:14 +0800

    :cat: init

---
+++ Anla/Controllers/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Anla.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public String Index()
+        {
+            return "page";
+        }
+    }
+}
