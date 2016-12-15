6addc6535b9eaab7b104ee1a3dd0be26c7f13890
zanjs <root@zanjs.com>
2016-12-15 18:03:12 +0800

    添加项目文件。

---
+++ Anla/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Anla.Startup))]
+namespace Anla
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
